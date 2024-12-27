e954a48ad712ad929bfdae4bca7f79fb04b25903
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-27 11:48:32 +0700

    feat(basic-routing): route named

--- routes/web.php
+++ routes/web.php
@@ -23,14 +23,16 @@ Route::view('/about','about',[
 // 1 parameter route
 // required parameter
 Route::get('/product/{id}', function(string $id){
+    $routes = route('route-name');
+    dd($routes);
     return "Product $id";
 });
 
 // 2 parameter route
 // required parameter
-Route::get('{lang}/product/{id}', function(string $lang, string $id){
-    return "Product $id in $lang";
-});
+// Route::get('{lang}/product/{id}', function(string $lang, string $id){
+//     return "Product $id in $lang";
+// });
 
 // optional parameter route
 // it mean the parameter is optional
@@ -38,3 +40,22 @@ Route::get('{lang}/product/{id}', function(string $lang, string $id){
 Route::get('/product/{category?}', function(string $category = null){
    return "Product $category";
 });
+
+
+// required parameter with regex pattern
+Route::get('/user/{username}', function(string $username){
+    return "User $username";
+})->where('username', '[a-z]+');// regex pattern for username only lower case otherwise it will throw error
+
+Route::get('{lang}/product/{id}', function(string $lang, string $id){
+    return "Product $id in $lang";
+})->where(['lang' => '[a-z]{2}', 'id' => '\d{4,}']);
+
+Route::get('/search/{search}', function(string $search){
+    return "Search $search";
+})->where('search', '.*');
+
+//route name
+Route::get('route-name-22', function(){
+    return "Route Name";
+})->name('route-name');
