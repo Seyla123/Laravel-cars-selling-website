2ab65a8ab7012b27ce6b4bd307effdc32734d3c8
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-27 16:48:25 +0700

    feat(home) : add home controller and route

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MathController;
-// define route for home page
-Route::get('/', function () {
-    return view('welcome');
-});
 
-// math
-
-Route::prefix('math')->group(function () {
-    Route::get('/sum/{a}/{b}', [MathController::class, 'sum'])->whereNumber(['a', 'b']);
-    Route::get('/subtract/{a}/{b}', [MathController::class, 'subtract']);
-});
+// Home
+Route::get('/', [HomeController::class, 'index'])->name('home');
