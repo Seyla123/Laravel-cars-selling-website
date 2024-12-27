20601722acefee0442f4fa0794c497725d3f2707
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-27 11:14:58 +0700

    feat(basic-routing): write comment

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 
+// define route for home page
 Route::get('/', function () {
     $person = [
         'name' => 'John Doe',
         'age' => 30
     ];
+    //dump($person); //for debug laravel built-in
+    // dump and die , mean dump and exit the script
     dd($person);
+    // return view
     return view('welcome');
 });
 
+// using view directly and pass data
 Route::view('/about','about',[
     'name' => 'John Doe',
 ]);
 
+// 1 parameter route
 Route::get('/product/{id}', function(string $id){
     return "Product $id";
 });
 
+// 2 parameter route
 Route::get('{lang}/product/{id}', function(string $lang, string $id){
     return "Product $id in $lang";
 });
