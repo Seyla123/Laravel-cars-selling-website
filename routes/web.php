d9f36d087c2ee8613b1eb36e6428a07d76b68933
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-27 14:51:28 +0700

    feat(basic-controller): resource and group resouce controller in route

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,20 @@
 <?php
 
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+
+// define route for home page
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::resource('/products', ProductController::class);
+// Route::apiResource('/cars', CarController::class);
+
+//can group api like this
+
+Route::apiResources([
+    'cars' => CarController::class,
+    'products' => ProductController::class
+]);
