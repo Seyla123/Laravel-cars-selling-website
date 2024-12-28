1eee24b5644b62aa3aa68182af235c6ad02d2c54
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-28 13:05:12 +0700

    chore(template) : clean layouts

--- resources/views/layouts/clean.blade.php
+++ resources/views/layouts/clean.blade.php
@@ -14,11 +14,11 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
 </head>
 
 <body class="font-sans antialiased">
     {{-- content render here --}}
-    @yield('content')
+    @yield('childContent')
 </body>
 </html>
