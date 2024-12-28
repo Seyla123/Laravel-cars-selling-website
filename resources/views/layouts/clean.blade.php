3263b2f9c26c8caaa916909098b1336e7b87064e
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-28 13:00:58 +0700

    feat(template) : add clean layouts

--- resources/views/layouts/clean.blade.php
+++ resources/views/layouts/clean.blade.php
@@ -18,11 +18,7 @@
 </head>
 
 <body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100">
-        <header>My Header</header>
-        {{-- content render here --}}
-        @yield('content')
-        <footer>My Footer</footer>
-    </div>
+    {{-- content render here --}}
+    @yield('content')
 </body>
 </html>
