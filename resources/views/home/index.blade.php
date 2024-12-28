a14dd729af5ba82117f1d6e61595838750a54651
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-28 10:03:19 +0700

    feat(view):Escaping

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,4 +1,13 @@
 <h1>Home index page</h1>
-<p>{{$name}}</p>
+<p>name {{strtoupper($name)}}</p>
 <p>{{$age}}</p>
 <p>{{$year}}</p>
+
+
+@verbatim
+<p>{{name}}</p>
+<p>{{age}}</p>
+<p>{{year}}</p>
+@if
+@endverbatim
+
