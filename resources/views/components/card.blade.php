293506544c4503f635386266d48a00c14e10bf52
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-28 20:30:49 +0700

    feat(component) : add component using slot and name slot

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,6 @@
-<div>
-    this card component
+<div class="bg-main-400 shadow-lg rounded-lg p-6">
+    <div class="text-xl font-semibold mb-4">{{ $title }}</div>
+    <div class="mb-4">{{ $slot }}</div>
+    <div class="text-xl font-semibold mb-4">{{ $footer }}</div>
 </div>
+
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,13 +6,22 @@
 
 {{-- start the content --}}
 @section('content')
+<x-card>
+    <x-slot name="title">
+        <h1>Card Title</h1>
+    </x-slot>
+    this card component
+    <x-slot name="footer">
+       <h1> card footer</h1>
+    </x-slot>
+    
+</x-card>
 <x-button >
     submit button
 </x-button>
 <x-button >
     another button
 </x-button>
-<x-auth.form-title />
     <h1>Home Page</h1>
 <x-button />
     <p>Home Page content goes here</p>
