7cd6d6468318174fe0cfb6f0f7c837fa0d697100
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-29 15:35:14 +0700

    feat: add logo to head html

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+    <link rel="icon" type="image/x-icon" href="{{ asset('/assets/images/seyla-logo.png')}}">
     <title>{{ $title }} | {{ config('app.name', 'Car Selling Website') }}</title>
 
     <!-- Fonts -->
@@ -16,7 +16,7 @@
 
     <!-- Scripts -->
     {{-- tailwind --}}
-    <script src="https://cdn.tailwindcss.com"></script> 
+    <script src="https://cdn.tailwindcss.com"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
