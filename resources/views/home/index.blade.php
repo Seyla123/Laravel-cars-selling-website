bcd8f8dd2cf7a53b23f5b0dc0d60fab7cf9c2ce9
Seav Seyla <Mrrseyla.758@gmail.com>
2025-01-01 11:59:19 +0700

    chore(index-car) : add pagination ui

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,7 +3,7 @@
     <div class="bg-slate-100 px-4 xl:px-12 py-4">
         <div class="max-w-screen-2xl mx-auto ">
             <x-home.search-form/>
-            <section>
+            <section class="space-y-2">
                 <h1 class="text-3xl font-bold py-4 md:py-6 ">Lastest Added Cars</h1>
                 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
                     @foreach ($cars as $key => $value)
@@ -16,6 +16,10 @@
                         :favorite="$value['favorite']"
                     />
                     @endforeach
+                </div>
+                {{-- pagination --}}
+                <div class="bg-white dark:bg-gray-800 flex justify-center items-center py-2 md:py-4 rounded-lg">
+                    <x-pagination totalPage="5" selectedPage="1" />
                 </div>
             </section>
         </div>
