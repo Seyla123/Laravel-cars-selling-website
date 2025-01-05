0ffef93c1840167779083961b70499c9d9143544
Seav Seyla <Mrrseyla.758@gmail.com>
2025-01-05 15:46:14 +0700

    chore(menu) : add route name to menu navbar

--- resources/views/components/layouts/menu.blade.php
+++ resources/views/components/layouts/menu.blade.php
@@ -8,7 +8,7 @@
             <a href="{{ route('car.create') }}" class="text-gray-800 hover:text-gray-600">
 
                 <x-button title="Add new car"
-                    customClass="duration-500 flex gap-2 items-center w-full py-3 px-6  text-sm tracking-wider border-main-600 border-2 font-semibold text-main-600  hover:bg-main-600 hover:text-white focus:outline-none rounded-full transition-transform transform hover:scale-110 animate-spin-slow ">
+                    customClass="duration-500 flex gap-2 items-center w-full py-3 px-6  text-sm tracking-wider border-main-600 border-2 font-semibold text-main-600  hover:bg-main-600 hover:text-white focus:outline-none rounded-full transition-transform transform hover:scale-110 animate-spin-slow group ">
                     <x-slot:leftIcon>
                         <svg data-lucide="circle-plus" class="w-6 h-6 "></svg>
                     </x-slot:leftIcon>
@@ -34,7 +34,7 @@
         </li>
         <li>
             {{-- signup --}}
-            <a href="/signup">
+            <a href="{{route('signup')}}">
                 <x-button title="Signup" class="duration-500 rounded-full w-full py-4 px-7 flex gap-2 items-center transition-transform transform hover:scale-110 animate-spin-slow ">
                     <x-slot:leftIcon>
                         <svg data-lucide="user-plus" class="w-5 h-5 "></svg>
@@ -44,7 +44,7 @@
         </li>
         <li>
             {{-- login --}}
-            <a href="/login">
+            <a href="{{route('login')}}">
                 <x-button title="Login" customClass="bg-white duration-300  flex gap-2 items-center hover:px-1  hover:text-main-700 hover:font-bold group">
                     <x-slot:leftIcon>
                         <svg data-lucide="log-out" class="w-5 h-5 transition-transform duration-500 group-hover:rotate-[360deg] transform"></svg>
