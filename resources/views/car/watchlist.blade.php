eb4797041ba2a2e5109b49758da8ea7e363a9572
Seav Seyla <Mrrseyla.758@gmail.com>
2025-01-06 10:03:21 +0700

    chore(watchlist): pass favourite varaible to car card component

--- resources/views/car/watchlist.blade.php
+++ resources/views/car/watchlist.blade.php
@@ -6,7 +6,7 @@
                 @if ($cars->count() > 0)
                     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
                         @foreach ($cars as $car )
-                            <x-car-card :$car />
+                            <x-car-card :$car :favorite='true'/>
                         @endforeach
                     </div>
                 @else
