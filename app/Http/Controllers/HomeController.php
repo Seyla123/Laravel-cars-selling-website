8881ca5200cc7e214a60b48a912618b5bc682e33
Seav Seyla <Mrrseyla.758@gmail.com>
2025-01-06 10:17:31 +0700

    chore(home-controller) : omptimaze query

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,19 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Car;
-use App\Models\CarType;
-use App\Models\City;
-use App\Models\FuelType;
-use App\Models\Maker;
-use App\Models\Model;
-use App\Models\State;
-use App\Models\User;
 
 class HomeController extends Controller
 {
     public function index()
     {
         $cars = Car::where('published_at', '<', now())
+                ->with('primaryImage','model', 'city', 'maker', 'carType', 'fuelType')
                 ->orderBy('published_at', 'desc')
                 ->limit(30)
                 ->get();
