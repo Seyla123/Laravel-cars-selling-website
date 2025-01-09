5bc667ba57390d4d43cc17d88983cc9b77bd0f28
Seav Seyla <Mrrseyla.758@gmail.com>
2025-01-09 15:27:30 +0700

    fix(menu) : fix small screen menu leaked sidebar

--- resources/views/components/layouts/mobile-menu.blade.php
+++ resources/views/components/layouts/mobile-menu.blade.php
@@ -4,7 +4,7 @@
 <div class="block md:hidden">
     <svg data-lucide="align-justify" class="w-8 h-8 group cursor-pointer" onclick="toggleMenu()"></svg>
 </div>
-<div id="mobileMenu" class="duration-500 top-0 right-0 fixed h-screen bg-white border -right-full z-50">
+<div id="mobileMenu" class="duration-500 top-0 right-0 -right-[100%] fixed md:hidden h-screen bg-white border  z-50">
     <div class="flex flex-col w-full h-full items-center justify-center p-8 bg-slate-200">
         <div class="w-full flex justify-end">
             <svg data-lucide="x" class="w-8 h-8 group cursor-pointer" onclick="toggleMenu()"></svg>
@@ -60,10 +60,10 @@
 
         if (toggle) {
             menu.classList.remove('w-screen');
-            menu.classList.add('-right-full');
+            menu.classList.add('-right-[100%]');
         } else {
             menu.classList.add('w-screen');
-            menu.classList.remove('-right-full');
+            menu.classList.remove('-right-[100%]');
         }
         toggle = !toggle;
 
